52f464a177d0c624554a56822cd1813069281225
Marcos Souza <marcos.souza@beeleads.com.br>
2024-02-13 12:54:17 -0300

    feat: pagina sign-in

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,29 @@
+import { Flame } from 'lucide-react'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Atom labs! - Login',
+  description: 'Your smart system',
+}
+
+export default function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="grid min-h-screen grid-cols-2 antialiased">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-1 text-lg text-foreground">
+          <Flame className="h-6 w-6" />
+          <span className="font-semibold tracking-tight">Atom Labs!</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy;atom.labs - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className="relative flex flex-col items-center justify-center">
+        {children}
+      </div>
+    </div>
+  )
+}
---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+export default function SignIn() {
+  return (
+    <div className="p-8">
+      <Button variant="ghost" asChild className="absolute right-8 top-8">
+        <Link href="sign-up">Novo estabelecimento</Link>
+      </Button>
+
+      <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Acompanhe seus pedidos pelo painel do parceiro
+          </p>
+        </div>
+
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input id="email" type="email" />
+          </div>
+
+          <Button className="w-full" type="submit">
+            Acessar painel
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
+}
